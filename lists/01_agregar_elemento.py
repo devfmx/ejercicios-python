59bd51c652a1c21b3452b4e05ce2cb5edc4e6138
Brian Salomaki <brian@salomaki.com>
2017-04-05 20:23:58 -0500

    Agregar lists:01

---
+++ lists/01_agregar_elemento.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+############################################################
+### Ejercicio: Agregar unos elementos nuevos a la lista. ###
+### Queremos agregar "GT" y "CO" a la lista.             ###
+############################################################
+
+my_list = ["MX", "US", "CN", "CA"]
+
+##################################################
+### Sólo modificar codigo abajo de esta linea. ###
+##################################################
+
+###################################################
+### Sólo modificar codigo arriba de esta linea. ###
+###################################################
+
+assert "GT" in my_list, "my_list debería tener 'GT'"
+assert "CO" in my_list, "my_list debería tener 'CO'"
+assert len(my_list) == 6, "mylist debería tener 6 elementos"
+
+print("Éxito! Felicidades!")
